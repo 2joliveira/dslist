8c863399edf0ce70eeed30931d8be8cd362e94ff
Jefferson Oliveira <oliveira2112jj@outlook.com>
2025-05-20 17:08:27 -0300

    first commit

---
+++ src/main/java/com/javaprojects/dslist/DslistApplication.java
@@ -0,0 +1,13 @@
+package com.javaprojects.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistApplication.class, args);
+	}
+
+}
