2d3fe901474c68a185c59a3454d21e7c6b8afe0f
Jefferson Oliveira <oliveira2112jj@outlook.com>
2025-05-24 13:21:17 -0300

    replacement game on list

---
+++ src/main/java/com/javaprojects/dslist/dto/ListReplacementDTO.java
@@ -0,0 +1,23 @@
+package com.javaprojects.dslist.dto;
+
+public class ListReplacementDTO {
+  private Integer sourceIndex;
+  private Integer detinationIndex;
+
+  public Integer getSourceIndex() {
+    return sourceIndex;
+  }
+
+  public void setSourceIndex(Integer sourceIndex) {
+    this.sourceIndex = sourceIndex;
+  }
+
+  public Integer getDetinationIndex() {
+    return detinationIndex;
+  }
+
+  public void setDetinationIndex(Integer detinationIndex) {
+    this.detinationIndex = detinationIndex;
+  }
+
+}
